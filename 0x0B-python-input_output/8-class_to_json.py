158689cd72fddc3e7b46f445525750c6490be2f1
Abderrahmane Benaamar <aboudiwac9@gmail.com>
2023-07-11 16:55:32 -0700

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
-[200~#!/usr/bin/python3
-        """Defines a Python class-to-JSON function."""
+#!/usr/bin/python3
+"""Defines a Python class-to-JSON function."""
 
 
-        def class_to_json(obj):
-            """Return the dictionary represntation of a simple data structure."""
-                return obj.__dict__
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
